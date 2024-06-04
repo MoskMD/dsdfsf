3ebba070262c6027a81c284a3e3e749711cef28c
ultra <ultra@DESKTOP-N49OHH3>
2024-06-04 07:26:53 +0300

    Add project files.

---
+++ Term1/Lab1/Lab1/Lab1.cpp
@@ -0,0 +1,19 @@
+#include <cstdio>
+
+int main() {
+    puts("Size of int: " + sizeof(int));
+    puts("Size of short: " + sizeof(short));
+    puts("Size of long: " + sizeof(long));
+    puts("Size of char: " + sizeof(char));
+    puts("Size of float: " + sizeof(float));
+    puts("Size of double: " + sizeof(double));
+
+    int intSize = sizeof(int);
+    int shortSize = sizeof(short);
+    int longSize = sizeof(long);
+    int charSize = sizeof(char);
+    int floatSize = sizeof(float);
+    int doubleSize = sizeof(double);
+
+    return 0;
+}
